11f008a3dd71b98bc106d40803d10ceebfb0fc40
Samuel Kiarie <skiarie21@gmail.com>
2023-02-20 08:45:57 -0800

    Software is eating the World two

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,21 +10,29 @@
 int main(void)
 {
 	int i = 0;
+	int f_d;
+	int l_d;
 	int j;
+	int f_d2;
+	int l_d2;
 
-	while (i < 100)
+	while (i <= 98)
 	{
+		f_d = (i / 10 + '0');
+		l_d = (i % 10 + '0');
 		j = 0;
-		while (j < 100)
+		while (j <= 99)
 		{
-			if (i < j && i != j)
+			f_d2 = (j / 10 + '0');
+			l_d2 = (j / 10 + '0');
+			if (i < j)
 			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
+				putchar(f_d);
+				putchar(l_d);
 				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j % 10 + '0');
-				if (i != 98)
+				putchar(f_d2);
+				putchar(l_d2);
+				if (c != 98)
 				{
 					putchar(',');
 					putchar(' ');
