731c0c23ff85383776a6e633df22f8680d0548a2
Samuel Kiarie <skiarie21@gmail.com>
2023-02-22 16:08:28 -0800

    number 4 correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 
 /**
  * _isalpha - main entry code
+ * @c: the character to be checked
  * a function that checks for alphabetic character
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
