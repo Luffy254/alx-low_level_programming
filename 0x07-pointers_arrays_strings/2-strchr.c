5ff2faa66aeb57e3cfc9366a22aeaaedee1634aa
Samuel Kiarie <skiarie21@gmail.com>
2023-03-06 17:54:44 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
