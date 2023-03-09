eca4beefbde45a45077c4e027919b47005c137d6
Samuel Kiarie <skiarie21@gmail.com>
2023-03-09 09:27:34 -0800

    0-puts_recursion.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,12 @@
  * main - C program that prints followed by new line
  * Return: 1
  */
+
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",59);
-	return (1);
-}
+	"and that piece of art is useful" - Dora Korpar,
+	2015 - 10 - 19\n",59);
+	
+	return (n);
+	
