98298451641b3711ffab1117b934011dd6b1600d
Samuel Kiarie <skiarie21@gmail.com>
2023-03-13 00:25:02 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,27 +7,41 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-	int n = 0;
-	int sign = 1;
+	int i, d, n, len, f, digit;
 
-	while (*s != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (*s == '-')
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign = -1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		else if (*s >= '0' && *s <= '9')
-		{
-			n = n * 10 + (*s - '0');
-		}
-		else if (n > 0)
-		{
-			break;
-		}
-		s++;
+		i++;
 	}
-	return (n * sign);
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
