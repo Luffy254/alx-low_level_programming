353ace9b39dd274769f7936bb47e49592a0d2950
Samuel Kiarie <skiarie21@gmail.com>
2023-03-13 00:21:30 -0700

    update 2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,19 +10,24 @@
 
 int _atoi(char *s)
 {
+	int n = 0;
 	int sign = 1;
-	int num = 0;
 
-	while (*s && (*s < '0' || *s > '9'))
+	while (*s != '\0')
 	{
 		if (*s == '-')
-			sign = -sign;
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			n = n * 10 + (*s - '0');
+		}
+		else if (n > 0)
+		{
+			break;
+		}
 		s++;
 	}
-	while (*s && (*s >= '0' && *s <= '9'))
-	{
-		num = num * 10 + (*s - '0');
-		s++;
-	}
-	return (sign * num);
+	return (n * sign);
 }
