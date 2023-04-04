9468bc374d7e28659eaeff3c47aea05f6a30da84
Samuel Kiarie <skiarie21@gmail.com>
2023-04-04 11:03:50 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - function that returns the nth node of a linked list
+ * @head: pointer to the head of the list
+ * @index: index of the node (starting from 0)
+ *
+ * Return: nth node or NULL
+ */
+
